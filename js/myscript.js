f02a63e5750e6f7a16dfd2b9be774f6d8fadd346
mdudek2 <mdudek2@hawk.iit.edu>
2024-02-28 02:03:27 -0600

    update to index.html style.css myscript.js
    reformatted code

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,13 @@
 jQuery(function ($) {
     $('html').removeClass('nojs');
     $("html").addClass("hasjs");
-    $("#altbox").on("click", function () {
+    $("#age-verification").on("click", function () {
         $("#age-verification").prop("checked", true);
         $("#altbox").html("<p>&#10004;</p>");
+		alert("click");
     });
+});
+
+jQuery(document).ready(function($) {
+alert("js is working");
 });
